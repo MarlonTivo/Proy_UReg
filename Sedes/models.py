158d06788f3d1d250249c51957001b4bee5f4583
Mtivo <marlonosieltivo@gmail.com>
2024-10-28 21:36:34 -0600

    Se agregro Asignacion,detalle asig. y sedes

--- Sedes/models.py
+++ Sedes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Sedes (models.Model):
+    Id_Sede= models.CharField (max_length=20, unique=True)
+    Nombre_Sede= models.CharField (max_length=75)
+    Coordinador= models.CharField (max_length=50)
+    Departamento= models.CharField (max_length=75)
+    Municipio= models.CharField (max_length=50)
+    Direcccion= models.CharField (max_length=75)
+    
+    def _str_(self):
+        return f"{self.Id_Sede} - {self.Nombre_Sede} - {self.Coordinador} - {self.Departamento} - {self.Municipio} - {self.Direcccion}"
